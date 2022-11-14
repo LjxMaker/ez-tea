9c7a2e2f0826dd7a88037e7f94f5fefc4188f125
kent <kent_1932596068@qq.com>
2022-11-14 10:35:56 +0800

    update time

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -41,11 +41,11 @@ Page({
       CurrentData.forEach(it => {
         let cur = it.shopCartData;
         let allNum = 0;
-        let allQuantity=0;
-        let Ctime = this.dealOngoingTime(it.shopCartData.length);
+        let allQuantity = 0;
+        let Ctime = this.dealOngoingTime(it.orderId);
         cur.forEach(el => {
           allNum = allNum + (el.price * el.quantity);
-          allQuantity=allQuantity+el.quantity;
+          allQuantity = allQuantity + el.quantity;
         });
         it = {
           ...it,
@@ -76,25 +76,21 @@ Page({
       console.log('请求数据失败', err);
     })
   },
-  dealOngoingTime(len) {
-    for (let i = 0; i < len; i++) {
-      let date = new Date();
-      let randomNumber = 0;
-      randomNumber = Math.floor(Math.random() * 30 + 1);
-      date.setMinutes(date.getMinutes() - randomNumber);
-      let year = date.getFullYear();
-      let month = date.getMonth()+1;
-      let day = date.getDate();
-      let hours = date.getHours();
-      let minute = date.getMinutes();
-      let second = date.getSeconds();
-      month=month<10?'0'+month:month;
-      day=day<10?'0'+day:day;
-      hours=hours<10?'0'+hours:hours;
-      second=second<10?'0'+second:second;
-      let time = year + '-' + month + '-' + day + ' ' + hours + ':' + minute + ':' + second;
-      return time;
-    }
+  dealOngoingTime(stTime) {
+    let date = new Date(Number(stTime.slice(4)));
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    hours = hours < 10 ? '0' + hours : hours;
+    minute = minute < 10 ? '0' + minute : minute;
+    second = second < 10 ? '0' + second : second;
+    let time = year + '-' + month + '-' + day + ' ' + hours + ':' + minute + ':' + second;
+    return time;
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
